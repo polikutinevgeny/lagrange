f2970d2f20aeff77177b1d876e3d65fe2d6c93e5
Polikutin Evgeny <polikutinevgeny@gmail.com>
2017-02-11 14:26:39 +1000

    Now it looks awful/great

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 from math import *
+from operator import mul
+from functools import reduce
 
 
 def lagrange(data):
-    def res(x):
-        s = 0
-        for j in range(len(data)):
-            p = data[j][1]
-            for i in range(len(data)):
-                if i != j:
-                    p *= (x - data[i][0]) / (data[j][0] - data[i][0])
-            s += p
-        return s
-    return res
+    return lambda x: sum([reduce(mul, [(x - data[i][0]) / (data[j][0] - data[i][0]) for i in range(len(data)) if i != j], 1) * data[j][1] for j in range(len(data))])
 
 
 def get_lagrange(func, begin, end, step):
